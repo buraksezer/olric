aefc88095710f489a318d0e1c48696beb2677582
Burak Sezer <burak.sezer.developer@gmail.com>
2020-08-22 15:53:48 +0300

    Run tests in transport and protocol packages

--- cover.sh
+++ cover.sh
@@ -9,8 +9,7 @@ set -e
 
 # create coverage output
 echo 'mode: atomic' > $OUT
-for PKG in $(go list ./...|grep -v -E 'vendor' | grep -v -E 'cmd' | 
-        grep -v -E 'bufpool' | grep -v -E 'protocol' | grep -v -E 'transport'); do
+for PKG in $(go list ./...|grep -v -E 'vendor'); do
   go test -covermode=atomic -coverprofile=$TMP $PKG
   tail -n +2 $TMP >> $OUT
 done
