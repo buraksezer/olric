f67f3e4b4256e435a98265b4019c1ac89f0d7035
Burak Sezer <burak.sezer.developer@gmail.com>
2020-08-23 11:51:18 +0300

    Exclude some packages in coverage

--- cover.sh
+++ cover.sh
@@ -9,7 +9,10 @@ set -e
 
 # create coverage output
 echo 'mode: atomic' > $OUT
-for PKG in $(go list ./...|grep -v -E 'vendor'); do
+for PKG in $(go list ./...| grep -v -E 'vendor'| grep -v -E 'cmd/olric-load'|
+ grep -v -E 'cmd/olric-stats'| grep -v -E 'cmd/olricd'|grep -v -E 'hasher'|
+ grep -v -E 'internal/bufpool'|grep -v -E 'cmd/discovery'|grep -v -E 'internal/flog'|
+ grep -v -E 'serializer'|grep -v -E 'stats'); do
   go test -covermode=atomic -coverprofile=$TMP $PKG
   tail -n +2 $TMP >> $OUT
 done
