8a0b622a617934fab0dc1a8f35d69cced5e72fa7
Burak Sezer <burak.sezer.developer@gmail.com>
2021-07-10 12:46:40 +0300

    feat: add pruneStaleTables

--- internal/storage/compaction.go
+++ internal/storage/compaction.go
@@ -15,21 +15,52 @@
 package storage
 
 import (
+	"fmt"
 	"log"
 )
+
+// pruneStaleTables removes stale tables from the table slice and make them available
+// for garbage collection. It always keeps the latest table.
+func (s *Storage) pruneStaleTables() {
+	var staleTables int
+
+	// Calculate how many tables are stale and eligible to remove.
+	// Always keep the latest table.
+	for _, t := range s.tables[:len(s.tables)-1] {
+		if len(t.hkeys) == 0 {
+			staleTables++
+		}
+	}
+
+	for i := 0; i < staleTables; i++ {
+		for i, t := range s.tables {
+			// See https://github.com/golang/go/wiki/SliceTricks
+			if len(t.hkeys) != 0 {
+				continue
+			}
+			copy(s.tables[i:], s.tables[i+1:])
+			s.tables[len(s.tables)-1] = nil // or the zero value of T
+			s.tables = s.tables[:len(s.tables)-1]
+			fmt.Println(">> Pruned stale table", t.allocated)
+			break
+		}
+	}
+}
 
 func (s *Storage) CompactTables() bool {
 	if len(s.tables) == 1 {
 		return true
 	}
 
-	var total int
-	fresh := s.tables[len(s.tables)-1]
-	for _, old := range s.tables[:len(s.tables)-1] {
+	defer s.pruneStaleTables()
+
+	var deleted int
+	latest := s.tables[len(s.tables)-1]
+	for _, prev := range s.tables[:len(s.tables)-1] {
 		// Removing keys while iterating on map is totally safe in Go.
-		for hkey := range old.hkeys {
-			entry, _ := old.getRaw(hkey)
-			err := fresh.putRaw(hkey, entry)
+		for hkey := range prev.hkeys {
+			entry, _ := prev.getRaw(hkey)
+			err := latest.putRaw(hkey, entry)
 			if err == errNotEnoughSpace {
 				// Create a new table and put the new k/v pair in it.
 				nt := newTable(s.Inuse() * 2)
@@ -42,23 +73,14 @@ func (s *Storage) CompactTables() bool {
 
 			// Dont check the returned val, it's useless because
 			// we are sure that the key is already there.
-			old.delete(hkey)
-			total++
-			if total > 1000 {
+			prev.delete(hkey)
+			deleted++
+			if deleted > 1000 {
 				// It's enough. Don't block the instance.
 				return false
 			}
 		}
 	}
 
-	// Remove empty tables. Keep the last table.
-	tmp := []*table{s.tables[len(s.tables)-1]}
-	for _, t := range s.tables[:len(s.tables)-1] {
-		if len(t.hkeys) == 0 {
-			continue
-		}
-		tmp = append(tmp, t)
-	}
-	s.tables = tmp
 	return true
 }
