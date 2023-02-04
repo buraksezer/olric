b300297cf80c3a7d750a1e0fca3eb7e64a3931f2
Burak Sezer <burak.sezer.developer@gmail.com>
2023-02-04 15:50:54 +0300

    fix: panic on start when BindAddr cannot be resolved #217

--- olric.go
+++ olric.go
@@ -57,7 +57,7 @@ import (
 )
 
 // ReleaseVersion is the current stable version of Olric
-const ReleaseVersion string = "0.5.3"
+const ReleaseVersion string = "0.5.4"
 
 var (
 	// ErrOperationTimeout is returned when an operation times out.
