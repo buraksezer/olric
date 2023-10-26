02a93247851df523003d711ae061b9e1f4db5f49
Burak Sezer <burak.sezer.developer@gmail.com>
2023-10-26 17:52:56 +0300

    chore: update ReleaseVersion

--- olric.go
+++ olric.go
@@ -57,7 +57,7 @@ import (
 )
 
 // ReleaseVersion is the current stable version of Olric
-const ReleaseVersion string = "0.5.4"
+const ReleaseVersion string = "0.6.0-alpha.1"
 
 var (
 	// ErrOperationTimeout is returned when an operation times out.
