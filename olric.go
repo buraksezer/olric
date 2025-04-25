961c360e85a9c870405f1744e7a71d744864dc08
Burak Sezer <burak.sezer.developer@gmail.com>
2025-04-25 17:49:57 +0300

    Update ReleaseVersion to 0.6.1
    Bumped the ReleaseVersion constant to reflect the latest stable release of Olric. This ensures versioning consistency and communicates the updated release.

--- olric.go
+++ olric.go
@@ -57,7 +57,7 @@ import (
 )
 
 // ReleaseVersion is the current stable version of Olric
-const ReleaseVersion string = "0.6.0"
+const ReleaseVersion string = "0.6.1"
 
 var (
 	// ErrOperationTimeout is returned when an operation times out.
